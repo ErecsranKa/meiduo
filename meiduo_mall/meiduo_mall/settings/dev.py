4a3439e6a9d5b1822762bac8b72724db17d858c1
ErecsranKa <pm5901cuxi01605@126.com>
2021-06-16 21:29:27 +0800

    用户模型类的创建和迁移

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 追加导包路径
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -32,13 +35,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth',  # 用户的认证系统
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'rest_framework',  # DRF
+
+    'users.apps.UsersConfig',  # 注册用户模块，为了适应AUTH_USER_MODEL，注册时以users开头
 
 ]
 
@@ -197,3 +202,6 @@ REST_FRAMEWORK = {
     # 异常处理
     'EXCEPTION_HANDLER': 'meiduo_mall.utils.exceptions.exception_handler',
 }
+
+# 指定User模型类为Django项目的用户模型类
+AUTH_USER_MODEL = 'users.User'  # 固定写法：应用.模型类
